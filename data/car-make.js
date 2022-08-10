7f50f5e53ce34a81a6d1df49bc69e4c46de63168
Teddy Kassa <103133862+Teddykassa2@users.noreply.github.com>
2022-08-10 17:20:28 +0000

    update

--- data/car-make.js
+++ data/car-make.js
@@ -49,71 +49,71 @@
 			  },
     		{
         "Car":5,
-				"Name":"Mustang",
+				"Name":"RS7",
 				"Year":2021,
-				"Engine":"5.2 V8",
-				"Trim":"GT500 Shelby",
-				"Make":"Ford",
-				"Horsepower":"760 HP 625 lb-ft ",
-				"Price":"$77,000.00",
-				"Image":"shelby.jpg"
+				"Engine":"4.0L V8",
+				"Trim":"Performance",
+				"Make":"Audi",
+				"Horsepower":"591 HP 590 lb-ft ",
+				"Price":"$118,500.00",
+				"Image":"rs7.jpg"
 			  },
         {    
 				"Car":6,
-				"Name":"Corvette C8",
+				"Name":"Aventador",
 				"Year":2021,
-				"Engine":"6.2 V8",
-				"Trim":"3LT",
-				"Make":"Chevrolet",
-				"Horsepower":"495 HP 465 lb-ft ",
-				"Price":"$77,000.00",
-				"Image":"c8.jpg"
+				"Engine":"6.5L V12",
+				"Trim":"SVJ",
+				"Make":"Lamborghini",
+				"Horsepower":"769 HP 531 lb-ft ",
+				"Price":"$517,770.00",
+				"Image":"svj.jpg"
 			  },
 			  {
           
         "Car":7,
-				"Name":"Challenger Hellcat",
+				"Name":"F12",
 				"Year":2021,
-				"Engine":"6.2 V8",
+				"Engine":"6.3L V12",
 				"Trim":"Red eye",
-				"Make":"Dodge",
-				"Horsepower":"807 HP 707 lb-ft ",
-				"Price":"$77,000.00",
-				"Image":"dr-no.jpg"
+				"Make":"Ferrari",
+				"Horsepower":"769 HP 520 lb-ft ",
+				"Price":"$621,600.00",
+				"Image":"f12.jpg"
 
 			  },
 			  {
         "Car":8,
-				"Name":"Mustang",
+				"Name":"Jesko",
 				"Year":2021,
-				"Engine":"5.2 V8",
-				"Trim":"GT500 Shelby",
-				"Make":"Ford",
-				"Horsepower":"760 HP 625 lb-ft ",
-				"Price":"$77,000.00",
-				"Image":"shelby.jpg"
+				"Engine":"5.1L V8",
+				"Trim":"Performance",
+				"Make":"Koenigsegg",
+				"Horsepower":"1,281 HP 1106 lb-ft ",
+				"Price":"$3,000,000.00",
+				"Image":"jesko.jpg"
 			  },
 			  {
         "Car":9,
-				"Name":"Camaro",
+				"Name":"E63",
 				"Year":2021,
-				"Engine":"6.2 V8",
-				"Trim":"ZL1",
-				"Make":"Chevy",
-				"Horsepower":"650 HP 650 lb-ft",
-				"Price":"$64,595.00",
-				"Image":"dr-no.jpg"
+				"Engine":"4.0L V8",
+				"Trim":"AMG",
+				"Make":"Mercedes",
+				"Horsepower":"603 HP 627 lb-ft",
+				"Price":"$108,500.00",
+				"Image":"e63.jpg"
 			  },
     		{
         "Car":10,
-				"Name":"Mustang",
+				"Name":"M5",
 				"Year":2021,
-				"Engine":"5.2 V8",
-				"Trim":"GT500 Shelby",
-				"Make":"Ford",
-				"Horsepower":"760 HP 625 lb-ft ",
-				"Price":"$77,000.00",
-				"Image":"shelby.jpg"
+				"Engine":"4.4L V8",
+				"Trim":"Competition",
+				"Make":"BMW",
+				"Horsepower":"617 HP 553 lb-ft ",
+				"Price":"$110,000.00",
+				"Image":"M5.jpg"
 			  }
 			]
 }
